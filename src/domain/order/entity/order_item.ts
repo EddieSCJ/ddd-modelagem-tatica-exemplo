e12d7786153ebedb799829b5e4628776a8fa0ad0
EddieSCJ <eddiestudyifs@gmail.com>
2023-02-19 04:59:17 -0300

    creating factories

--- src/domain/order/entity/order_item.ts
+++ src/domain/order/entity/order_item.ts
@@ -1,14 +1,12 @@
 export default class OrderItem {
     _id: string;
-    _orderId: string;
     _name: string;
     _productId: string;
     _quantity: number;
     _price: number;
 
-    constructor(public id: string, public name: string, public price: number, public productId: string, public quantity: number, public orderId: string) {
+    constructor(public id: string, public name: string, public price: number, public productId: string, public quantity: number) {
         this._id = id;
-        this._orderId = orderId;
         this._productId = productId;
         this._name = name;
         this._quantity = quantity;
@@ -24,10 +22,6 @@ export default class OrderItem {
     validateBasicOrderItem() {
         if (this._id == null || this._id == "") {
             throw new Error('Order Item ID must be provided and not empty');
-        }
-
-        if (this._orderId == null || this._orderId == "") {
-            throw new Error('Order Item must have an order');
         }
 
         if (this._name == null) {
